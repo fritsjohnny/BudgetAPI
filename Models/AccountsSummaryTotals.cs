f4f581ffd8c04b23b8311312af61f19e9e9a77a3
Johnny Frits <e-jfrits@ppi-multitask.com.br>
2025-03-03 11:10:32 -0400

    Adiciona propriedade ToReceive e atualiza configurações de publicação
    O arquivo `AccountsSummaryTotals.cs` agora inclui a propriedade `ToReceive` na classe `AccountsSummaryTotals`, com melhorias na formatação do código.
    
    No arquivo `BudgetAPI - Web Deploy1.pubxml`, foram feitas atualizações no `ResourceId`, `SiteUrlToLaunchAfterPublish`, `MSDeployServiceURL` e `DeployIisAppPath` para refletir um novo site no Azure. O campo `UserName` foi limpo para preenchimento futuro, enquanto outras configurações permaneceram inalteradas.

--- Models/AccountsSummaryTotals.cs
+++ Models/AccountsSummaryTotals.cs
@@ -1,13 +1,13 @@
 ﻿namespace BudgetAPI.Models
 {
-	public class AccountsSummaryTotals
-	{
-		public decimal ForecastBalance { get; set; }
-		public decimal AvailableBalance { get; set; }
-		public decimal ForecastSpared { get; set; }
-		public decimal AvailableSpared { get; set; }
-		public decimal DrawnBalance { get; set; }
-		public decimal WithoutDrawnBalance { get; set; }
-	}
-
+    public class AccountsSummaryTotals
+    {
+        public decimal ForecastBalance { get; set; }
+        public decimal AvailableBalance { get; set; }
+        public decimal ForecastSpared { get; set; }
+        public decimal AvailableSpared { get; set; }
+        public decimal DrawnBalance { get; set; }
+        public decimal WithoutDrawnBalance { get; set; }
+        public decimal ToReceive { get; set; }
+    }
 }
