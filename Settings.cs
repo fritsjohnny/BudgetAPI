85693c10f6dc388ea192e46ca8b0bae6448e9f10
Johnny Frits <johnny.frits@outlook.com>
2026-02-03 13:42:40 -0300

    Alterar segredo para variável de ambiente
    O valor do segredo (Secret) não é mais definido diretamente no código-fonte. Agora, ele é obtido da variável de ambiente "BUDGETAPI_SECRET", retornando uma string vazia caso não esteja definida. Essa alteração aumenta a segurança ao evitar exposição de informações sensíveis no repositório.

--- Settings.cs
+++ Settings.cs
@@ -1,7 +1,7 @@
 ﻿namespace BudgetAPI
 {
-	public static class Settings
-	{
-		public static string Secret = "?uQ<bdzj,.rM-&5SJpA6>CsRAE*^{SK3Szts&<cYHB:TX8f/T$s0YPz+GC08-w[+";
-	}
+    public static class Settings
+    {
+        public static string Secret => Environment.GetEnvironmentVariable("BUDGETAPI_SECRET") ?? string.Empty;
+    }
 }
