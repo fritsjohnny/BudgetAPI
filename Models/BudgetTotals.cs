9b667ba4b3da2ff814a81335e2acc902e41917ee
Johnny Frits <frits.johnny@gmail.com>
2026-05-10 16:24:45 -0300

    Implementados novos campos

--- Models/BudgetTotals.cs
+++ Models/BudgetTotals.cs
@@ -1,16 +1,20 @@
 ﻿namespace BudgetAPI.Models
 {
-	public class BudgetTotals
-	{
-		public decimal MyExpenses { get; set; }
-		public decimal MyExpensesPerc { get; set; }
-		public decimal OthersExpenses { get; set; }
-		public decimal OthersExpensesPerc { get; set; }
-		public decimal MyIncomes { get; set; }
-		public decimal MyIncomesPerc { get; set; }
-		public decimal OthersIncomes { get; set; }
-		public decimal OthersIncomesPerc { get; set; }
+    public class BudgetTotals
+    {
+        public decimal MyExpenses { get; set; }
+        public decimal MyExpensesPerc { get; set; }
+        public decimal OthersExpenses { get; set; }
+        public decimal OthersExpensesPerc { get; set; }
 
-	}
+        public decimal MyIncomes { get; set; }
+        public decimal MyIncomesPerc { get; set; }
+        public decimal OthersIncomes { get; set; }
+        public decimal OthersIncomesPerc { get; set; }
 
+        public decimal MyYields { get; set; }
+        public decimal MyYieldsPerc { get; set; }
+        public decimal MyIncomesWithoutYields { get; set; }
+        public decimal MyIncomesWithoutYieldsPerc { get; set; }
+    }
 }
